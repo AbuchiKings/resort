d25bab69c4e7303486ada8e6a37f3ae63e2a1d2e
Abuchi Kingsley <abuchikingsley76@gmail.com>
2020-01-20 15:13:40 +0100

    Added hero and banner components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import Hero from '../components/Hero';
+import Banner from './../components/Banner';
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
-        <div>
-Hello from home page
-        </div>
+        <Hero >
+            <Banner title="luxurious rooms"
+                subtitle="deluxe rooms starting at $299">
+                <Link to="/rooms" className="btn-primary">
+                    our rooms
+                </Link>
+            </Banner>
+        </Hero>
     )
 }
 
 export default Home;
+
+Hero.defaultProps = {
+    hero: "defaultHero"
+}
